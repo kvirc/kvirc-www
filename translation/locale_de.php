7e158193672f960496fd0f10a18f0f51cb5ffd0e
Markus Rennings <markus@rennings.net>
2009-12-09 20:20:24 +0000

    updated german translation wrt r3675

--- translation/locale_de.php
+++ translation/locale_de.php
@@ -987,7 +987,7 @@ $template['themes']['theme32']="...weil die ganze Welt ein schönes Fraktal ist!
 $template['themes']['theme33']="Ein dunkleres technisch aussehendes Thema mit einfachen und beschreibenden Symbolen";
 $template['themes']['theme34']="Dieses Thema wurde vom CoreZeam.org Portal erstellt";
 $template['themes']['theme35']="Ein schwarz-basiertes Thema, angenehm zu sehen und zu erkennen";
-$template['themes']['theme36']="A pink theme inspired on Hello Kitty";
+$template['themes']['theme36']="Ein rosafarbenes Thema inspiriert von »Hello Kitty«";
 /* themes.php end */
 
 /* tools.php start */
