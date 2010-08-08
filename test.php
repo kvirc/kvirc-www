fd503302583d9d5162cc5eaa3c02f7a26a71ecc4
Szymon Tomasz Stefanek <pragma@kvirc.net>
2010-08-08 15:38:37 +0000

    Fix the test.php script

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?
-$url = "https://svn.kvirc.de/kvirc/timeline";
+$url = "ssl://svn.kvirc.de";
 $handle = fsockopen($url,80,$errno,$errstr,30);
 if(!$handle)
 {
 	$error = "$errstr ($errno)";
 	die($error);
 } else {
-	$out = "GET / HTTP/1.1\r\n";
-	$out .= "Host: www.example.com\r\n";
+	$out = "GET /kvirc/timeline HTTP/1.1\r\n";
+	$out .= "Host: svn.kvirc.de\r\n";
 	$out .= "Connection: Close\r\n\r\n";
 	fwrite($handle,$out);
 	while(!feof($handle))
