3453313c84037258be9dc99bc7e79d6a380567cf
Elvio Basello <hellvis69@gmail.com>
2010-08-09 12:47:59 +0000

    upgraded checkversion.php script to allow checking of svn revisions

--- checkversion.php
+++ checkversion.php
@@ -7,5 +7,40 @@ unset($checkNetHack);
 unset($_GET['checkNetHack']);
 $checkNetHack=0;
 require("conf/config.php");
-echo $current;
+
+$svn = isset($_GET['svn']) ? trim(strip_tags(addslashes($_GET['svn']))) : "";
+if(($svn == "") || (!is_numeric($svn))) $svn = 0;
+
+if($svn == 0)
+{
+	$latest = $current;
+} else {
+	$handle = fsockopen("ssl://svn.kvirc.de",443,$errno,$errstr,30);
+	if(!$handle)
+	{
+		$error = "$errstr ($errno)";
+		die($error);
+	} else {
+		$out  = "GET /kvirc/timeline HTTP/1.1\r\n";
+		$out .= "Host: svn.kvirc.de\r\n";
+		$out .= "Connection: Close\r\n\r\n";
+		fwrite($handle,$out);
+		while(!feof($handle))
+		{
+			$tmp = fgets($handle,128);
+
+			// <span class="time">17:56</span> Changeset <em>[4813]</em>
+			$line = strip_tags(strstr($tmp,"Changeset"));
+			if(!$line) continue;
+			else {
+				// Changeset [4813] 
+				preg_match("/\d+/",$line,$match);
+				break;
+			}
+		}
+	}
+	fclose($handle);
+	$latest = $match[0];
+}
+echo $latest;
 ?>
