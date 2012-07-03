78ae7d92c036f452b5376688bf0efe55d1bdaa00
Szymon Tomasz Stefanek <pragma@kvirc.net>
2012-07-03 00:14:28 +0000

    Announce 4.2.0

--- content/welcome.php
+++ content/welcome.php
@@ -8,6 +8,6 @@ echo "<div class=\"column1-unit\">";
 	echo "<p>".$template['welcome']['text']."</p>";
 	echo "<p>&rsaquo;&rsaquo;&nbsp;<a href=\"?id=help&amp;lang=$lang\" title=\"".$template['welcome']['alttext']."\">".$template['welcome']['text2']."</a></p>";
 
-	echo "<div class=\"center\"><img src=\"img/keyhands.jpg\" alt=\"Using KVIrc\" /></div>";
+	echo "<div class=\"center\"><img style=\"float: right; border: 0px;\" src=\"img/keyhands.jpg\" alt=\"Using KVIrc\" /></div>";
 echo "</div>";
 ?>
