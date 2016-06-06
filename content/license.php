ba751394dedebfc16d15acea54fef08e8a9e05fa
un1versal <un1versal@users.noreply.github.com>
2016-06-06 20:27:36 +0100

    correct GPL lic address

--- content/license.php
+++ content/license.php
@@ -17,7 +17,7 @@ echo "<div class=\"column1-unit\">";
  Extended by Szymon Stefanek
 
  Copyright (C) 1989, 1991 Free Software Foundation, Inc.
- 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  Everyone is permitted to copy and distribute verbatim copies
  of this license document, but changing it is not allowed.
 
